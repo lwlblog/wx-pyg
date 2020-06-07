aef39a0dfbe68f1a123ed6808cddc08c625be8f4
liwenlong <1519523354@qq.com>
2020-06-07 14:22:08 +0800

    update api

--- request/index.js
+++ request/index.js
@@ -15,7 +15,7 @@ export const request = (params) => {
     title: '加载中',
     mask: true
   });
-  const baseUrl = 'https://api.zbztb.cn/api/public/v1';
+  const baseUrl = 'https://api-hmugo-web.itheima.net/api/public/v1';
   return new Promise((resolve, reject) => {
     wx.request({
       ...params,
